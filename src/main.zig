79c0328a99b4d580fb2d599c43ea62f5cc9d34f8
Joshua Jones <joshua@general-metrics.com>
2024-05-28 00:29:16 -0700

    Move file input to a cli arg instead of hard-coded const

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,20 @@ const Lexer = @import("./Lexer.zig");
 const Token = @import("./Token.zig");
 
 pub fn main() !void {
-    const input =
-        \\local firstName = "Joshua"
-        \\local lastName = 'Jones'
-        \\function name()
-        \\  return 1 + 1
-        \\end
-    ;
-    var lexer = Lexer.init(input);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const file_name = args[1];
+    const max_size = std.math.maxInt(usize);
+    const file = try std.fs.cwd().readFileAlloc(allocator, file_name, max_size);
+    defer allocator.free(file);
+
+    var lexer = Lexer.init(file);
     var i: u32 = 0;
-    while (i <= input.len) : (i += 1) {
+    while (i <= file.len) : (i += 1) {
         const token = lexer.advance().?;
         std.debug.print("Type: {?}\n", .{token});
         if (token.kind == Token.Kind.Eof) {
---
+++ test/simple.lua
@@ -0,0 +1,7 @@
+-- hi
+local firstName = "Joshua"
+local lastName = "Jones"
+local function add()
+	return 1 + 41
+end
+print(add())
